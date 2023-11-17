ebd2cdc2c4354a561ab07d05e6a0c239f5caf5cd
Nameless-itf23 <ohutoncat@gmail.com>
2023-11-17 10:40:43 +0900

    add tweet

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,30 @@ const tweets: string[] = [
   'https://twitter.com/Nameless_itf/status/1724371173344162274',
   'https://twitter.com/Nameless_itf/status/1724372013576560807',
   'https://twitter.com/Nameless_itf/status/1724430685421625670',
-  'https://twitter.com/Nameless_itf/status/1724432862303179007'
+  'https://twitter.com/Nameless_itf/status/1724432862303179007',
+  'https://twitter.com/Nameless_itf/status/1724433970727706876',
+  'https://twitter.com/Nameless_itf/status/1724630018448580895',
+  'https://twitter.com/Nameless_itf/status/1724635481852592399',
+  'https://twitter.com/Nameless_itf/status/1724646944008761696',
+  'https://twitter.com/Nameless_itf/status/1725072153303998608',
+  'https://twitter.com/Nameless_itf/status/1725138299227095446',
+  'https://twitter.com/Nameless_itf/status/1725138465959076280',
+  'https://twitter.com/Nameless_itf/status/1725138677108719974',
+  'https://twitter.com/Nameless_itf/status/1725138850144768176',
+  'https://twitter.com/Nameless_itf/status/1725139189975675307',
+  'https://twitter.com/Nameless_itf/status/1725139391537164684',
+  'https://twitter.com/Nameless_itf/status/1725139616695713830',
+  'https://twitter.com/Nameless_itf/status/1725139773822792114',
+  'https://twitter.com/Nameless_itf/status/1725324322737459567',
+  'https://twitter.com/Nameless_itf/status/1725325028278083680',
+  'https://twitter.com/Nameless_itf/status/1725325195983061318',
+  'https://twitter.com/Nameless_itf/status/1725325532253032704',
+  'https://twitter.com/Nameless_itf/status/1725326059481272466',
+  'https://twitter.com/Nameless_itf/status/1725326275429167555',
+  'https://twitter.com/Nameless_itf/status/1725326345469890616',
+  'https://twitter.com/Nameless_itf/status/1725326767429492856',
+  'https://twitter.com/Nameless_itf/status/1725326878259732676',
+  'https://twitter.com/Nameless_itf/status/1725326922740318712'
 ];
 
 const pr = new Intl.PluralRules("en-US", { type: "ordinal" });
@@ -38,6 +61,7 @@ const Header: React.FC = () => {
   return (
     <header>
       <h1 className='gradation'>#コンテンツ入門2023</h1>
+      <p>読み込みまで時間がかかることがあります。ツイートが表示されない場合でも、そのまましばらくお待ちください...</p>
     </header>
   )
 }
